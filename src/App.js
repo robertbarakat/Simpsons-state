ffd4a3ae8b0a7c77365db2c9cf589634487935fb
robertbarakat <robert.barakat@yahoo.it>
2018-10-08 14:53:27 +0200

    Quete Simpsons

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quote from "./Quote";
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      working: true
+    }
+  }
+
+  toggle(){
+    this.setState({ working: !this.state.working });
+  }
+          
+  render() {
+    const light = this.state.working ? "App-logo" : "App-logo1";
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={light} alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+        </header>
+        <button onClick={() => this.toggle()}>Change State</button>
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
